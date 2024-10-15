296c1e31d741a117397bbff3e036e1c04bc4fbd7
EzequielJesusHerreraNegrin <159430518+EzequielJesusHerreraNegrin@users.noreply.github.com>
2024-10-15 19:32:25 +0100

    feat: implement an algorithm to read txt file.

--- src/App.java
+++ src/App.java
@@ -1,8 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
 public class App {
 
     public static void main(String[] args) {
-        
+        final String INPUT_ROUTE = "./src/input.txt";
+
+        ArrayList<String> phrases = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(INPUT_ROUTE, StandardCharsets.UTF_8))) {
+            String currentLine = reader.readLine();
+            while (currentLine != null) {
+                phrases.add(currentLine);
+                currentLine = reader.readLine();
+            }
+            phrases.remove(0);
+            reader.close();
+        } catch (Exception e) {
+            System.out.println("Error: no se puede extraer el contenido de: " + INPUT_ROUTE);
+        }
     }
-
-
 }
